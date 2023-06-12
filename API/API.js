12933bb5c81e3df39c758d25f32574ed9323b15e
Andra <andramaria080@gmail.com>
2023-06-12 13:05:01 +0300

    API.js edited

--- API/API.js
+++ API/API.js
@@ -6,22 +6,26 @@ window.onload = () => {
   getAdvice();
 };
 
-function getAdvice() {
-  fetch("https://api.adviceslip.com/advice")
-    .then((run) => {
-      return run.json();
-    })
-    .then((advData) => {
-      const tips = advData.slip;
-      adviceRun.innerHTML = `<p>"${tips.advice}"</p>`;
-      adviceNum.innerHTML = `<span>#${tips.id}</span>`;
-    })
-
-    .catch((error) => {
-      console.log(error);
-    });
+function randomAdvice(advice) {
+  adviceRun.textContent = advice;
+}
+function randomNumber(number) {
+  adviceNum.textContent = "#" + number;
 }
 
-adviceBtn.addEventListener("click", () => {
-  getAdvice();
-});
+async function getAdvice() {
+  const url = "https://api.adviceslip.com/advice";
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    let advice = data.slip.advice;
+    let number = data.slip.id;
+    randomAdvice(advice);
+    randomNumber(number);
+  } catch (error) {
+    console.log(error);
+  }
+}
+adviceBtn.addEventListener("click", getAdvice);
